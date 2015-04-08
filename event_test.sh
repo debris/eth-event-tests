ee42613a9487f5fa1f208cf2735611c6377266c4
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-04-08 10:22:44 +0200

    creating contract

--- event_test.sh
+++ event_test.sh
@@ -3,8 +3,14 @@
 export solc="/Users/marekkotewicz/ethereum/cpp-ethereum/build/solc/Debug/solc"
 RPCPORT=8545
 
+echo "getting coinbase..."
+COINBASE=`curl -s -X POST --data '{"jsonrpc": "2.0","method": "eth_coinbase", "params": [],"id": 1}' http://localhost:${RPCPORT} | grep result | cut -d : -f 2 | cut -d '"' -f 2`
+echo "coinbase: ${COINBASE}"
+
+echo "preparing tmp directory..."
 rm -rf tmp && mkdir tmp && cd $_
 
+echo "creating Test0 contract..."
 echo 'contract Test0 {
     event Hello();
 
@@ -13,7 +19,19 @@ echo 'contract Test0 {
     }
 }' > Test0.sol
 
+
+echo "creating binary and abi files..."
 solc --input-file Test0.sol --json-abi file --binary file --add-std 0
 
-echo ${code1}
+TEST0_BINARY=`cat Test0.binary`
+echo "test0 binary: ${TEST0_BINARY}"
+
+TX="{\"jsonrpc\": \"2.0\",\"method\": \"eth_sendTransaction\", \"params\": [{\"from\": \"${COINBASE}\", \"data\": \"${TEST0_BINARY}\"}],\"id\": 1}"
+echo "${TX}"
+ADDRESS=`curl -s -X POST --data "${TX}" http://localhost:${RPCPORT} | grep result | cut -d : -f 2 | cut -d '"' -f 2`
+echo "created contract at: ${ADDRESS}"
+
+echo "Writing csv file!"
+echo "Test0,${ADDRESS}" > contracts.csv
+echo "Finished!"
 
